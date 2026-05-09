e6d6744c2f8dcb6b39611feaec5dc2236fcbe613
beatnickbonz <beatnickbonz@gmail.com>
2026-05-09 15:55:12 -0500

    Add files via upload

---
+++ ART/src/scenes/background.js
@@ -0,0 +1,19 @@
+// "Every great game begins with a single scene. Let's make this one unforgettable!"
+export class Background extends Phaser.Scene {
+    constructor() {
+        super('Background');
+    }
+
+    init() {
+        // Initialize scene
+    }
+
+    preload() {
+        // Load assets
+    }
+
+    create() {
+        // Create game objects
+    }
+
+}
